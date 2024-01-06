4a058e9b66d6029732c4a472b580ee0a966f79b8
uehara1107 <u.sumika1107@gmail.com>
2024-01-06 17:19:12 +0900

    [add]connect db
    mysqlで作成したDBと接続できるようにした

--- src/main/java/com/baseball/demo/DemoController.java
+++ src/main/java/com/baseball/demo/DemoController.java
@@ -2,21 +2,35 @@ package com.baseball.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 
 @RestController
 public class DemoController {
-    @RequestMapping("/test")
-    public OutputData get(){
-        OutputData data = new OutputData();
-        data.id = 123;
-        data.value = "hello";
-        data.date = new Date();
-        return data;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(path = "/test", method = RequestMethod.GET)
+    public String get(){
+        List<Map<String, Object>> list;
+        list = jdbcTemplate.queryForList("select * from my_table");
+        return list.toString();
+    }
+
+    @RequestMapping(path = "/test/{id}", method = RequestMethod.GET)
+    public String read(@PathVariable String id) {
+        List<Map<String, Object>> list;
+        list = jdbcTemplate.queryForList("select * from my_table where id = ?", id);
+        return list.toString();
     }
     
 }
